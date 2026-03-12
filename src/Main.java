b7b83143db710e290be0446e5ae10cff74677396
santiago <santiagoandresramirezlucas@gmail.com>
2026-03-12 13:50:09 -0500

    Ejercicios

--- src/Main.java
+++ src/Main.java
@@ -13,17 +13,17 @@ public class Main{
         System.out.println("D1    D2");
         System.out.println(d1+"     "+d2);
     }
-        System.out.println("Suma dado numero uno:" + s1);
-        System.out.println("Suma dado numero dos:" + s2 );
+        System.out.println("Suma dado número uno:" + s1);
+        System.out.println("Suma dado número dos:" + s2 );
         if(s1>s2){
             System.out.println("dado 1 es el ganador\n"  );
         } else if (s2>s1) {
-            System.out.println("dado 2 es el gandor\n");
+            System.out.println("dado 2 es el ganador\n");
         } else {
             System.out.println("Empate\n");
         }
 
-        //----------Ejerccio 2----------//
+        //----------Ejercicio 2----------//
     Scanner lector=new Scanner(System.in);
         int intentos;
         int al;
