b6010b7c7bfb4fb9fa20ddeb6ba23d7410af693b
santiago <santiagoandresramirezlucas@gmail.com>
2026-03-12 14:58:01 -0500

    Ejerccios

--- src/Main.java
+++ src/Main.java
@@ -47,7 +47,95 @@ public class Main{
             System.out.println("Perdió");
         }
     }
+    //----------Ejercicio 3----------//
+        System.out.println("Juego de la ruleta");
+    int bala = (int) (Math.random() * 5+1);
+    boolean e1= false;
+    boolean e2= false;
+    boolean e3= false;
+    boolean e4= false;
+    boolean e5= false;
+    switch (bala){
+        case 1:
+            e1= true;
+            break;
+        case 2:
+            e2= true;
+            break;
+        case 3:
+            e3= true;
+            break;
+        case 4:
+            e4= true;
+            break;
+        case 5:
+            e5= true;
+            break;
+    }
+    for(int i =0; i <=5; i++) {
+        if (e1 == true) {
+            System.out.println("persona 1 dispara y muere");
+            break;
+        } else {
+            System.out.println("persona 1 dispara y sobrevive");
+        }
+        if (e2 == true) {
+            System.out.println("persona 2 dispara y muere");
+            break;
+        } else {
+            System.out.println("persona 2 dispara y sobrevive");
+        }
+        if (e3 == true) {
+            System.out.println("persona 3 dispara y muere");
+            break;
+        } else {
+            System.out.println("persona 3 dispara y sobrevive");
+        }
+        if (e4 == true) {
+            System.out.println("persona 4 dispara y muere");
+            break;
+        } else {
+            System.out.println("persona 4 dispara y sobrevive");
+        }
+        if (e5 == true) {
+            System.out.println("persona 5 dispara y muere");
+            break;
+        } else {
+            System.out.println("persona 5 dispara y sobrevive");
+        }
+    }
+        System.out.println(" ");
+    //----------Ejercicio 3 mejorado----------//
 
+        System.out.print("Escibir cantidad de personas que quieren jugar: ");
+    int Pnumero= lector.nextInt();
+    int bala2 = (int) (Math.random() * 5+1);
+    int disparar= bala2;
+    int cartucho=1;
+
+    boolean muerto= false;
+
+    while(muerto!=true) {
+        for (int i= 1; i <=Pnumero;i++){
+            System.out.println("Turno del cartucho: "+ cartucho);
+            System.out.println("Vala en cartucho: " + disparar);
+            if (disparar == cartucho) {
+                System.out.println("Persona " + i + " dispara y muere");
+                muerto = true;
+                break;
+            } else {
+                System.out.println("Persona " + i + " dispara y sobrevive");
+                if (cartucho >= 5) {
+                    cartucho = 1;
+                } else {
+                    cartucho += 1;
+                }
+                bala2 = (int) (Math.random() * 5 + 1);
+                disparar = bala2;
+            }
+
+            }
+    }
 
     }
 }
